b8d70b7444e79ed66a3901b2673eb61f46cc9f22
Aliot <aliot@tut.by>
2015-10-29 14:37:58 +0300

    footer-header create

---
+++ wp-content/themes/theme 2/footer.php
@@ -0,0 +1,22 @@
+<div class = "footer-wrapper">
+	<div class = "footer-main">
+		<div class = "footer-left">
+			<a href = "/"><img src = "<?php bloginfo('template_url');?>/images/logo-ftr.png" alt = "" /></a>
+			<p>copyright 2015 <a href = "#">www.yoururl.com</a></p>
+		</div>
+		<div class = "footer-right">
+			<a href = "/"><img src = "<?php bloginfo('template_url');?>/images/twitter.jpg" alt = "" /></a>		
+			<a href = "/"><img src = "<?php bloginfo('template_url');?>/images/facebook.jpg" alt = "" /></a>		
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquerypp.custom.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.elastislide.js"></script>
+<script type="text/javascript">
+	
+	$( '#carousel' ).elastislide();
+												
+</script>
+</body>
+</html>
---
+++ wp-content/themes/theme 2/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel = "stylesheet" type = "text/css"; href = "<?php bloginfo('stylesheet_url'); ?>" />
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/elastislide.css" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/custom.css" />
+<script src="<?php bloginfo('template_url'); ?>/js/modernizr.custom.17475.js"></script>
+<!--[if gte IE 9]>
+  <style type="text/css">
+    .gradient {
+       filter: none;
+    }
+  </style>
+<![endif]-->
+<!--[if lte IE 9]>
+  <style type="text/css">
+	.body{
+		background:url(<?php bloginfo('template_url');?>/images/body-bg-ie.png) repeat-x 0 730px;
+	}
+	.menu, .elastislide-wrapper{
+		background:url(<?php bloginfo('template_url');?>/images/opacity.png);
+	}	
+	.active{
+  background:url(<?php bloginfo('template_url');?>/images/fonie.png) no-repeat top center; background-size: 100%;
+    filter:progid:DXImageTransform.Microsoft.AlphaImageLoader( src='<?php bloginfo('template_url');?>/images/fonie.png', sizingMethod='scale');
+ }
+  </style>
+<![endif]-->
+<title>Fashion Photographer</title>
+</head>
+<body>
+<div class = "karkas">
+	<div class = "header">
+		<a href ="/"><img src = "<?php bloginfo('template_url');?>/images/logo.png" class = "logo" alt = "Fashion Photographer" /></a>
+		<ul class = "menu">
+			<li><a href="/" class="active">HOME</a></li>
+			<li><a href="about.html">ABOUT ME</a></li>
+			<li><a href="portfolio.html">PORTFOLIO</a></li>
+			<li><a href="portfolio1.html">PORTFOLIO-GALLERY</a></li>
+			<li><a href="exhibition.html">EXHIBITIONS</a></li>
+			<li><a href="contact.html">CONTACT ME</a></li>
+		</ul>
+	</div>
