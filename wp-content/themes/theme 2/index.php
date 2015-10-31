97879589fc1d5986e1edc2cc90c421b11f5ffc92
Aliot <aliot@tut.by>
2015-10-31 19:53:28 +0300

    page and posts on the index page

--- wp-content/themes/theme 2/index.php
+++ wp-content/themes/theme 2/index.php
@@ -7,50 +7,26 @@
 		<li>For Slider</li>
 	</ul>
 <?php endif;?>
-		<!--<ul id="carousel" class="elastislide-list">
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel1.png" alt="image01" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel2.png" alt="image02" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel3.png" alt="image03" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel4.png" alt="image04" /></a></li>	
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel1.png" alt="image01" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel2.png" alt="image02" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel3.png" alt="image03" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel4.png" alt="image04" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel1.png" alt="image01" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel2.png" alt="image02" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel3.png" alt="image03" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel4.png" alt="image04" /></a></li>	
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel1.png" alt="image01" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel2.png" alt="image02" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel3.png" alt="image03" /></a></li>
-			<li><a href="#"><img src="<?php bloginfo('template_url');?>/images/small/karusel4.png" alt="image04" /></a></li>			
-		</ul>-->
+		
 		<!-- End Elastislide Carousel -->
 		<div class = "content-main-block">
-			<div>
-				<a href = "#"><img src = "<?php bloginfo('template_url');?>/images/main-block1.jpg" alt = "" /></a>
-				<h2><a href = "#">Hi, My Name Is Anne Photographer</a></h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisi odio, sollicitudin at condimentum at, mattis a ligula.
-				</p>
-				<p>Ut vitae urna quis massa consectetur cursus ultricies in tellus. Aliquam sed odio in neque mattis laoreet. Cras lacus purus,
-				</p>
-				<p><a href = "#" class = "readmore">read more</a></p>
-			</div>
-			<div>
-				<a href = "#"><img src = "<?php bloginfo('template_url');?>/images/main-block2.jpg" alt = "" /></a>
-				<h2><a href = "#">Exhibition Now On</a></h2>
-				<p>Nam egestas sapien molestie lorem auctor eget scelerisque neque tincidunt. Ut iaculis leo non sem can <a href = "#">this is an inline link.</a> Maecenas mi massa, interdum at sagittis eu, imperdiet a dui. Duis sit amet tortor leo. Fusce tempor pellentesque mollis. Sed tincidunt elit sit amet ipsum fermentum venenatis.
-				</p>
-				<p><a href = "#" class = "readmore">read more</a></p>
-			</div>
-			<div>
-				<a href = "#"><img src = "<?php bloginfo('template_url');?>/images/main-block3.jpg" alt = "" /></a>
-				<h2><a href = "#">Latest Photo Shoot</a></h2>
-				<p>Donec mollis suscipit nisi, vitae sagittis arcu euismod at. In at turpis vel odio fringilla vulputate at ac tellus. Phasellus nulla leo, vehicula sit amet congue eget, viverra ut augue epharetra aliquet turpis vel scelerisque. Nullam ut enim nisi. Vivamus ut neque nulla. est aliquam consectetur augue et est dapibus ;
-				</p>				
-				<p><a href = "#" class = "readmore">read more</a></p>
-			</div>
-			
+<?php $args = array('post_type' => array('page', 'post'), 
+					'meta_key' => 'order',
+					'orderby' => 'meta_value_num',
+					'order' => 'ASC',
+					'posts_per_page' => 3); ?>		
+<?php $page_index = new WP_Query($args);?>
+
+<?php if ($page_index->have_posts()) :  while ($page_index->have_posts()) : $page_index->the_post(); ?>
+<div>
+	<!--<a href = "<?php the_permalink();?>"><img src = "<?php bloginfo('template_url');?>/images/main-block1.jpg" alt = "" /></a>-->
+	<h2><a href = "<?php the_permalink();?>"><?php echo get_post_meta(get_the_ID(), 'title', true); ?></a></h2>
+	<?php the_excerpt(); ?>
+	<p><a href = "<?php the_permalink();?>" class = "readmore">read more</a></p>
+</div>       
+<?php endwhile; ?>
+<?php endif; ?>
+		
 		</div>				
 	</div>	
 </div>
